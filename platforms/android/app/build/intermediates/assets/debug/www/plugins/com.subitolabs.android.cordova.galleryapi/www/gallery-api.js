873def6063464a9c5fd92856803a04b995e63a24
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2019-02-11 22:36:26 +0700

    first commit

---
+++ platforms/android/app/build/intermediates/assets/debug/www/plugins/com.subitolabs.android.cordova.galleryapi/www/gallery-api.js
@@ -0,0 +1,61 @@
+cordova.define("com.subitolabs.android.cordova.galleryapi.galleryAPI", function(require, exports, module) {
+
+function GalleryAPI()
+{
+    
+}
+
+GalleryAPI.prototype.checkPermission = function(successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'checkPermission',
+        []
+    );
+};
+
+
+GalleryAPI.prototype.getAlbums = function(successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'getAlbums',
+        []
+    );
+};
+
+GalleryAPI.prototype.getMedia = function(albumName, successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'getMedia',
+        [albumName]
+    );
+};
+
+GalleryAPI.prototype.getMediaThumbnail = function(media, successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'getMediaThumbnail',
+        [media]
+    );
+};
+
+GalleryAPI.prototype.getHQImageData = function(media, successCallback, errorCallback) {
+    cordova.exec(
+        successCallback,
+        errorCallback,
+        'GalleryAPI',
+        'getHQImageData',
+        [media]
+    );
+};
+
+module.exports = new GalleryAPI();
+
+});
